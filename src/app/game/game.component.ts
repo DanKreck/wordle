846715cf4931fe3af7c92d3c19757acc577f5b7e
Chris Bay <chris@chrisbay.net>
2022-08-05 10:13:08 -0500

    Basic wordle implementation

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -23,25 +23,69 @@ export class GameComponent implements OnInit {
 
   handleGuess(event: any): void {
 
-  }
+    let guess = event.target.value.toUpperCase();
 
-  isLetterMatch(letter: string): boolean {
-    let isMatch = false;
-    
+    // make sure the guess has exactly 5 characters
+    if (guess.length !== this.WORD_LENGTH) {
+      return;
+    }
 
-    return isMatch;
+    // put the guess in this.rows
+    this.rows.push(guess);
+
+    // did they get answer?
+    if (guess === this.solution) {
+      setTimeout(function(){
+        alert("You won!");
+      }, 0); 
+    }
+
+    // is this the final guess? 
+    if (this.rows.length === this.NUM_ROWS && guess !== this.solution) {
+      setTimeout(function(){
+        alert("You lost!");
+      }, 0); 
+    }
+
+    if (guess === this.solution || this.rows.length === this.NUM_ROWS) {
+      event.target.disabled = true;
+    }
+
+    event.target.value = '';
   }
 
   isGuess(letter: string): boolean {
     let isGuess = false;
     
+    for (let i: number = 0; i < this.rows.length; i++) {
+      if (this.rows[i].includes(letter)) {
+        isGuess = true;
+        break;
+      }
+    }
 
     return isGuess;
   }
 
-  isPositionMatch(letter: string): boolean {
+  isLetterMatch(letter: string): boolean {
     let isMatch = false;
     
+    if (this.isGuess(letter) && this.solution.includes(letter)) {
+      isMatch = true;
+    }
+
+    return isMatch;
+  }
+
+  isLocationMatch(letter: string): boolean {
+    let isMatch = false;
+    
+    for (let i: number = 0; i < this.rows.length; i++) {
+      if (this.rows[i].includes(letter) && this.rows[i].indexOf(letter) === this.solution.indexOf(letter)) {
+        isMatch = true;
+        break;
+      }
+    }
 
     return isMatch;
   }
